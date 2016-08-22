4900c73e7e413252d83189915bae21130fc436fe
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-08-22 10:10:12 +0300

    feat: make ut-port-jsonrpc inherit from ut-port-http

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-var errors = require('./errors');
-module.exports = {
-    id: 'jsonrpc',
-    createPort: require('ut-port-http'),
-    url: global.window && global.window.location.origin,
-    raw: {
-        json: true,
-        jar: true,
-        strictSSL: false
-    },
-    parseResponse: false,
-    requestTimeout: 300000,
-    receive: function(msg, $meta) {
-        if ($meta.mtid === 'error') {
-            return msg;
-        }
-        if (msg && msg.payload) {
-            if (msg.payload.result) {
-                return msg.payload.result;
-            } else if (msg.payload.error) {
-                throw msg.payload.error;
+var HttpPort = require('ut-port-http');
+var util = require('util');
+var errors = require('./errors.js');
+
+function JsonRpcPort() {
+    HttpPort.call(this);
+    this.config = {
+        id: 'jsonrpc',
+        url: global.window && global.window.location.origin,
+        raw: {
+            json: true,
+            jar: true,
+            strictSSL: false
+        },
+        parseResponse: false,
+        requestTimeout: 300000,
+        receive: function(msg, $meta) {
+            if ($meta.mtid === 'error') {
+                return msg;
             }
-            throw errors.wrongJsonRpcFormat(msg);
-        }
-        throw errors.generic(msg);
-    },
-    send: function(msg, $meta) {
-        var result = {
-            uri: (msg && msg.uri) || `/rpc/${$meta.method}`,
-            payload: {
-                id: 1,
-                jsonrpc: '2.0',
-                method: $meta.method,
-                params: msg
+            if (msg && msg.payload) {
+                if (msg.payload.result) {
+                    return msg.payload.result;
+                } else if (msg.payload.error) {
+                    throw msg.payload.error;
+                }
+                throw errors.wrongJsonRpcFormat(msg);
             }
-        };
-        if ($meta.method === 'identity.check' && !msg.uri) {
-            result.uri = '/login';
+            throw errors.generic(msg);
+        },
+        send: function(msg, $meta) {
+            var result = {
+                uri: (msg && msg.uri) || `/rpc/${$meta.method}`,
+                payload: {
+                    id: 1,
+                    jsonrpc: '2.0',
+                    method: $meta.method,
+                    params: msg
+                }
+            };
+            if ($meta.method === 'identity.check' && !msg.uri) {
+                result.uri = '/login';
+            }
+            return result;
         }
-        return result;
-    }
-};
+    };
+}
+
+util.inherits(JsonRpcPort, HttpPort);
+
+module.exports = JsonRpcPort;
