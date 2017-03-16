abdee22fdf37158d680bb01f333cc42c3bb32d7a
stambata <stamen.peev@softwaregroup-bg.com>
2017-03-16 15:39:51 +0200

    feat: always preserve json-rpc errors (#4)

--- errors.js
+++ errors.js
@@ -4,11 +4,15 @@ var create = utError.define;
 var RPC = create('PortRPC');
 var Generic = create('Generic', RPC);
 var WrongJsonRpcFormat = create('WrongJsonRpcFormat', RPC);
+var plainError = cause => Object.assign(new Error(), cause);
 
 module.exports = {
     rpc: function(cause) {
-        var error = cause && cause.type && utError.get(cause.type);
-        return (error && error(cause)) || new RPC(cause);
+        var error = RPC;
+        if (cause && cause.type) {
+            error = utError.get(cause.type) || plainError;
+        }
+        return error(cause);
     },
     generic: function(cause) {
         return new Generic(cause);
