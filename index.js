3bbbffc7ea62c8d8b0abc10afb879038529b89bb
Ranjithkumar Pandiyan <ranjith.pandiyan@softwaregroup-bg.com>
2018-04-23 13:51:41 +0530

    feat: blob type response (#22)

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ module.exports = function(...params) {
                     httpMethod: ($http && $http.httpMethod) || 'POST',
                     headers: ($http && $http.headers),
                     requestTimeout: timeout,
+                    blob: ($http && $http.blob),
                     payload: {
                         id: ($meta.mtid === 'request') ? requestId++ : null,
                         jsonrpc: '2.0',
