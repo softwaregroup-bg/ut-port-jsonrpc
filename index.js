59c4b37ee982e8c7f2fd48c3478905de24d148cc
Kalin Krustev <kalin.krustev@gmail.com>
2018-12-05 08:59:55 +1000

    fix: refactor as class (#27)
    BREAKING CHANGE: requires upgrade of ut-port and ut-run

--- index.js
+++ index.js
@@ -1,77 +1,75 @@
 'use strict';
 const HttpPort = require('ut-port-http');
-const util = require('util');
-const merge = require('lodash.merge');
 
 module.exports = function(...params) {
-    let parent = HttpPort(...params);
     let jsonRpcPortErrors;
 
-    function JsonRpcPort() {
-        parent && parent.apply(this, arguments);
-        if (!this.errors || !this.errors.getError) throw new Error('Please use the latest version of ut-port');
-        jsonRpcPortErrors = jsonRpcPortErrors || require('./errors')(this.errors);
-        let requestId = 1;
-
-        this.config = merge(this.config, {
-            url: global.window && global.window.location.origin,
-            raw: {
-                json: true,
-                jar: true,
-                strictSSL: false
-            },
-            parseResponse: false,
-            requestTimeout: 300000,
-            minLatency: 100,
-            receive: function(msg, $meta) {
-                if ($meta.mtid === 'error') {
-                    return msg;
-                }
-                if (msg && msg.payload) {
-                    if (msg.payload.result !== undefined && msg.payload.error === undefined) {
-                        if (msg.payload.id == null) {
-                            $meta.mtid = 'discard';
+    return class JsonRpcPort extends HttpPort(...params) {
+        constructor() {
+            super(...arguments);
+            if (!this.errors || !this.errors.getError) throw new Error('Please use the latest version of ut-port');
+            jsonRpcPortErrors = jsonRpcPortErrors || require('./errors')(this.errors);
+            this.requestId = 1;
+        }
+        get defaults() {
+            return {
+                url: global.window && global.window.location.origin,
+                raw: {
+                    json: true,
+                    jar: true,
+                    strictSSL: false
+                },
+                parseResponse: false,
+                requestTimeout: 300000,
+                minLatency: 100
+            };
+        }
+        handlers() {
+            return {
+                receive: (msg, $meta) => {
+                    if ($meta.mtid === 'error') {
+                        return msg;
+                    }
+                    if (msg && msg.payload) {
+                        if (msg.payload.result !== undefined && msg.payload.error === undefined) {
+                            if (msg.payload.id == null) {
+                                $meta.mtid = 'discard';
+                            }
+                            return msg.payload.result;
+                        } else if (typeof msg.payload.error === 'object') {
+                            throw jsonRpcPortErrors.rpc(msg.payload.error);
                         }
-                        return msg.payload.result;
-                    } else if (typeof msg.payload.error === 'object') {
-                        throw jsonRpcPortErrors.rpc(msg.payload.error);
+                        throw jsonRpcPortErrors.wrongJsonRpcFormat(msg);
                     }
-                    throw jsonRpcPortErrors.wrongJsonRpcFormat(msg);
-                }
-                throw jsonRpcPortErrors.generic(msg);
-            },
-            send: function(msg, $meta) {
-                let timeout = $meta.timeout && this.timing && Math.floor(this.timing.diff(this.timing.now(), $meta.timeout));
-                if (Number.isFinite(timeout) && timeout <= this.config.minLatency) throw this.errors.timeout();
-                let $http = (msg && msg.$http) || {};
-                let result = {
-                    uri: $http.uri || `/rpc/${$meta.method.replace(/\//ig, '%2F')}`,
-                    url: $http.url,
-                    withCredentials: $http.withCredentials,
-                    httpMethod: $http.httpMethod || 'POST',
-                    headers: $http.headers,
-                    requestTimeout: timeout,
-                    blob: $http.blob,
-                    payload: {
-                        jsonrpc: '2.0',
-                        method: $meta.method,
-                        timeout: timeout && (timeout - this.config.minLatency),
-                        params: (msg && !(msg instanceof Array) && Object.assign({}, msg)) || msg
+                    throw jsonRpcPortErrors.generic(msg);
+                },
+                send: (msg, $meta) => {
+                    let timeout = $meta.timeout && this.timing && Math.floor(this.timing.diff(this.timing.now(), $meta.timeout));
+                    if (Number.isFinite(timeout) && timeout <= this.config.minLatency) throw this.errors.timeout();
+                    let $http = (msg && msg.$http) || {};
+                    let result = {
+                        uri: $http.uri || `/rpc/${$meta.method.replace(/\//ig, '%2F')}`,
+                        url: $http.url,
+                        withCredentials: $http.withCredentials,
+                        httpMethod: $http.httpMethod || 'POST',
+                        headers: $http.headers,
+                        requestTimeout: timeout,
+                        blob: $http.blob,
+                        payload: {
+                            jsonrpc: '2.0',
+                            method: $meta.method,
+                            timeout: timeout && (timeout - this.config.minLatency),
+                            params: (msg && !(msg instanceof Array) && Object.assign({}, msg)) || msg
+                        }
+                    };
+
+                    if ($http.mtid !== 'notification' && $meta.mtid === 'request') {
+                        result.payload.id = this.requestId++;
                     }
-                };
-
-                if ($http.mtid !== 'notification' && $meta.mtid === 'request') {
-                    result.payload.id = requestId++;
+                    if ($http) delete result.payload.params.$http;
+                    return result;
                 }
-                if ($http) delete result.payload.params.$http;
-                return result;
-            }
-        });
-    }
-
-    if (parent) {
-        util.inherits(JsonRpcPort, parent);
-    }
-
-    return JsonRpcPort;
+            };
+        }
+    };
 };
