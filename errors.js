a378ddb4bc07796888bbfe8290c1edf17234fe46
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2020-07-16 17:31:20 +0300

    fix: preserve received error message

--- errors.js
+++ errors.js
@@ -2,16 +2,9 @@
 module.exports = ({defineError, getError}) => {
     const RPC = defineError('portJsonRPC', undefined, 'JSON RPC port error');
     const Generic = defineError('generic', RPC, 'Generic JSON RPC port error');
-    const plainError = cause => Object.assign(Generic(), cause);
 
     return {
-        rpc: function(cause) {
-            let error = RPC;
-            if (cause && cause.type) {
-                error = getError(cause.type) || plainError;
-            }
-            return error(cause);
-        },
+        rpc: cause => Object.assign(Generic(), cause),
         generic: Generic,
         wrongJsonRpcFormat: defineError('wrongJsonRpcFormat', RPC, 'Wrong JSON RPC format')
     };
